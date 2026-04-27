5e89e3543ecc33e67f2eaf894a7212d7c96d6934
Mike <mikevdburgt2@gmail.com>
2026-04-27 13:11:11 +0200

    Fix Vercel build errors

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
+  // Dit schakelt de errors uit die de build laten falen
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   output: 'standalone',
   images: {
     unoptimized: true,
