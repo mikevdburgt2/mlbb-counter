a1e27855dcf7b75a0f33d3d0c71cd4d2ae18dfdd
Mike <mikevdburgt2@gmail.com>
2026-04-27 13:06:17 +0200

    Fix build for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'standalone', // Dit helpt vaak bij Vercel
+  images: {
+    unoptimized: true, // Tijdelijk voor simpele sites
+  },
 };
 
 export default nextConfig;
