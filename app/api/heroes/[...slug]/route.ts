4f6143292e5117674959df21388f320ddbbf814f
Mike <mikevdburgt2@gmail.com>
2026-04-27 13:46:38 +0200

    Simplify API route to fix build

--- app/api/heroes/[...slug]/route.ts
+++ app/api/heroes/[...slug]/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({ message: 'API route working' });
+  return NextResponse.json({
+    message: 'Hero API is working',
+  });
 }
